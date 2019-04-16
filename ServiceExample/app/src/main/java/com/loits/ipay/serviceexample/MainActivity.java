80273b4de79b053e0307f347a3eec194d12e8bec
Yasas <yasass@lolctech.com>
2019-04-16 11:02:23 +0530

    project add

---
+++ ServiceExample/app/src/main/java/com/loits/ipay/serviceexample/MainActivity.java
@@ -0,0 +1,30 @@
+package com.loits.ipay.serviceexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Myservice.class);
+                startService(i);
+            }
+        });
+
+
+
+    }
+}
